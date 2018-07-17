97424a5ea5bcf0ac5ea51f6a29554c3ad72394eb
Alexander Biryukov <alex.biryukoff@gmail.com>
2018-07-17 23:52:03 +0700

    Create homebrew.sh for dotfiles

---
+++ homebrew.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+main() {
+  #install_homebrew
+  install_brew_packages
+}
+
+install_homebrew() {
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+function install_brew_packages() {
+  # Install other useful binaries
+    brew install git
+    brew install git-lfs
+    brew install tig
+    brew install nvm
+    brew install tmux
+    brew install midnight-commander
+    #brew install fzf
+    brew install unrar
+
+
+    brew install geckodriver
+    brew install selenium-server-standalone
+
+  # Cask
+    brew cask install flux
+    brew cask install caffeine
+    brew cask install chromium
+    brew cask install firefox
+    brew cask install vlc
+    brew cask install chromedriver
+
+  # Services
+    brew services
+ 
+  # Cleanup
+    brew cleanup
+    brew cask cleanup
+}
+
+main "$@"
